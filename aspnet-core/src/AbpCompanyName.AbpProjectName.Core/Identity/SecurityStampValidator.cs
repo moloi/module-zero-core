c685eab801d41efaf2ddc0d96933cf6c43f9c349
Moloi Sam <ntibilengm@gmail.com>
2020-03-09 06:31:31 +0200

    first commit

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Identity/SecurityStampValidator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Abp.Authorization;
+using AbpCompanyName.AbpProjectName.Authorization.Roles;
+using AbpCompanyName.AbpProjectName.Authorization.Users;
+using AbpCompanyName.AbpProjectName.MultiTenancy;
+using Microsoft.Extensions.Logging;
+
+namespace AbpCompanyName.AbpProjectName.Identity
+{
+    public class SecurityStampValidator : AbpSecurityStampValidator<Tenant, Role, User>
+    {
+        public SecurityStampValidator(
+            IOptions<SecurityStampValidatorOptions> options,
+            SignInManager signInManager,
+            ISystemClock systemClock,
+            ILoggerFactory loggerFactory) 
+            : base(options, signInManager, systemClock, loggerFactory)
+        {
+        }
+    }
+}
